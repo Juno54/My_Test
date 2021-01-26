48886f5762333b55dd00c36b7b62aee65a2de0f3
Juno54 <xuzhouzhou54@163.com>
2021-01-26 22:12:21 +0800

    练习pytest

--- 20210126pytest1.py
+++ 20210126pytest1.py
@@ -1,5 +1,34 @@
+import pytest
+
+
 def func(x):
     return x+1
 
-def test_answer():
-    assert func(3)== 5
+##参数化
+@pytest.mark.parametrize('a,b',[(1,2),(2,3),(20,40),('a','a1'),(5,9)])
+def test_answer1(a,b):
+    assert func(a)== b
+
+def test_answer2():
+    assert func(3) == 4
+
+def test_answer3():
+    assert func(3)== 3
+
+###装饰器
+@pytest.fixture()
+def login():
+    print("登录操作")
+    username='JunoXu'
+    return username
+
+class TestDemo:
+    def test_a(self,login):
+        ##前面加个f是什么意思？？？？
+        print(f"a   username={login}")
+
+    def test_b(self):
+        print("b")
+
+    def test_c(self,login):
+        print("c  username={login}")
