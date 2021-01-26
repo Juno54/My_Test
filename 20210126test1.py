1b818f22adc43fd941c2e256083575b785e6d85b
Juno54 <xuzhouzhou54@163.com>
2021-01-26 10:02:01 +0800

    20210126：练习循环

---
+++ 20210126test1.py
@@ -0,0 +1,15 @@
+###猜数游戏，用while循环
+
+import random
+
+computer_number=random.randint(1,100)
+while True:
+    person_number=(int(input("请输入1个数字:")))
+    print(computer_number)
+    if person_number>computer_number:
+        print('太大了')
+    elif person_number<computer_number:
+        print('太小了')
+    elif person_number == computer_number:
+        print('猜对啦')
+        break
